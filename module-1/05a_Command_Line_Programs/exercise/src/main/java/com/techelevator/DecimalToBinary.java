7ca391817b2d729271813015af738fcee1e61386
Brandon Perez <8randonperez@gmail.com>
2022-09-19 12:04:50 -0400

    the most beautiful decimal to binary code *everrr*

--- module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05a_Command_Line_Programs/exercise/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,17 +1,28 @@
 package com.techelevator;
-
 import java.util.Scanner;
-
 public class DecimalToBinary {
-
 	public static void main(String[] args) {
-
 		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Please list some decimal numbers to convert to binary: ");
-
-
-		
+		System.out.print("Please list some decimal numbers to convert to binary: ");
+		String unconvertedLine = sc.nextLine();
+		String[] strList = unconvertedLine.split(" ");
+		System.out.println();
+		for (int i = 0; i < strList.length; i++) {
+			System.out.println(strList[i] + " in binary is "  + binaryBarfer(Double.parseDouble(strList[i])));
+		} //IT'S ALL SO BEAUTIFUL *crying emoji*
 	}
-
+	public static String binaryBarfer(double og){
+		String binaryReturn = "0";
+		while(og!=0){
+			int binaryPow;
+			for(binaryPow = 0; Math.pow(2, binaryPow+1)<=og; binaryPow++ ){}
+			og-=Math.pow(2, binaryPow);
+			String newAddition = "1";
+			for(binaryPow = binaryPow; binaryPow!=0; binaryPow--){
+				newAddition = newAddition+ "0";
+			}
+			binaryReturn = (binaryReturn.equals("0")) ? binaryReturn = newAddition : binaryReturn.substring(0, binaryReturn.length()-newAddition.length()) + newAddition;
+		}
+		return binaryReturn;
+	}
 }
