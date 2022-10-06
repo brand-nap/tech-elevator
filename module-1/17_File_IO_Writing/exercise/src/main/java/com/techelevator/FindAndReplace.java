c1ab0d9ba46f8dea871bf4a051e3e3c58fdd5bf9
Brandon Perez <8randonperez@gmail.com>
2022-10-05 21:20:39 -0400

    Finished two tests well. Passed out

--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FindAndReplace.java
@@ -1,9 +1,30 @@
 package com.techelevator;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class FindAndReplace {
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String search = scanner.nextLine();
+        String replace = scanner.nextLine();
+        File source = new File(scanner.nextLine());
+        File destination = new File(scanner.nextLine());
+        destination.createNewFile();
+        try(PrintWriter fileWriter = new PrintWriter(destination)){
+            Scanner fileReader = new Scanner(source);
+            while(fileReader.hasNextLine()){
+                String[] line = fileReader.nextLine().split( " ");
+                for(int i = 0; i < line.length-1; i++){
+                    line[i] = (line[i].equals(search)) ? replace : (line[i].equals(search + ",")) ? replace + "," : line[i];
+                    fileWriter.print(line[i] + " ");
+                }
+                fileWriter.println(line[line.length-1]);;
+            }
+        }
     }
-
 }
--- module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ module-1/17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,16 @@
 package com.techelevator;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class FizzWriter {
-
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws IOException {
+		try(PrintWriter fileWriter = new PrintWriter(new File(new Scanner(System.in).nextLine()))){
+			for (int i = 1; i < 301; i++) {
+				String str = (i%5 == 0 && i%3 == 0) ? "FizzBuzz": (i%5 == 0) ? "Buzz": (i%3 == 0) ? "Fizz": ""+i;
+				fileWriter.println(str);
+			}
+		}
 	}
-
 }
