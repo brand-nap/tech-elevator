a5221baa0b94c34a1578d33a8e1005a3c18266a1
Brandon Perez <8randonperez@gmail.com>
2022-10-27 08:19:20 -0400

    Fixed and elaborated on solutions for DAO testing

--- module-2/08_DAO_Testing/exercise/src/main/java/com/techelevator/projects/dao/JdbcTimesheetDao.java
+++ module-2/08_DAO_Testing/exercise/src/main/java/com/techelevator/projects/dao/JdbcTimesheetDao.java
@@ -37,7 +37,7 @@ public class JdbcTimesheetDao implements TimesheetDao {
                      "WHERE employee_id = ? " +
                      "ORDER BY timesheet_id;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, employeeId);
-        if (results.next()) {
+        while (results.next()) {
             Timesheet timesheet = mapRowToTimesheet(results);
             timesheets.add(timesheet);
         }
@@ -49,7 +49,7 @@ public class JdbcTimesheetDao implements TimesheetDao {
         List<Timesheet> timesheets = new ArrayList<>();
         String sql = "SELECT timesheet_id, employee_id, project_id, date_worked, hours_worked, billable, description " +
                      "FROM timesheet " +
-                     "WHERE employee_id = ? " +
+                     "WHERE project_id = ? " +
                      "ORDER BY timesheet_id;";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, projectId);
         while (results.next()) {
@@ -90,7 +90,7 @@ public class JdbcTimesheetDao implements TimesheetDao {
         double billableHours = 0;
         String sql = "SELECT SUM(hours_worked) AS billable_hours " +
                      "FROM timesheet " +
-                     "WHERE employee_id = ? AND project_id = ?";
+                     "WHERE employee_id = ? AND project_id = ? AND billable = true";
         SqlRowSet results = jdbcTemplate.queryForRowSet(sql, employeeId, projectId);
         if (results.next()) {
             billableHours = results.getDouble("billable_hours");
--- module-2/08_DAO_Testing/exercise/src/test/java/com/techelevator/projects/dao/JdbcTimesheetDaoTests.java
+++ module-2/08_DAO_Testing/exercise/src/test/java/com/techelevator/projects/dao/JdbcTimesheetDaoTests.java
@@ -43,29 +43,53 @@ public class JdbcTimesheetDaoTests extends BaseDaoTests {
     @Test
     public void getTimesheetsByEmployeeId_returns_list_of_all_timesheets_for_employee() {
         int timesheets = dao.getTimesheetsByEmployeeId(1).size();
-        assertEquals(timesheets, 2);
+        boolean inOrder = true;
+        int lastID = 0;
+        for (Timesheet timesheet : dao.getTimesheetsByEmployeeId(1)) {
+            inOrder = (lastID < timesheet.getTimesheetId()) ? inOrder : false;
+        }
+
+        assertEquals(2, timesheets);
+        assertEquals(true, inOrder);
     }
 
     @Test
     public void getTimesheetsByProjectId_returns_list_of_all_timesheets_for_project() {
         int timesheets = dao.getTimesheetsByProjectId(1).size();
-        assertEquals(timesheets, 3);
+        boolean inOrder = true;
+        int lastID = 0;
+        for (int i = 0; i < 10 ; i++) {
+            for (Timesheet timesheet : dao.getTimesheetsByProjectId(i)) {
+                inOrder = (lastID < timesheet.getTimesheetId()) ? inOrder : false;
+            }
+        }
+        assertEquals(3, timesheets);
+        assertEquals(true, inOrder);
     }
 
     @Test
     public void createTimesheet_returns_timesheet_with_id_and_expected_values() {
         Timesheet timesheet = dao.createTimesheet(TIMESHEET_3);
+        assertEquals(5, timesheet.getTimesheetId());
+        timesheet.setTimesheetId(3);
         assertTimesheetsMatch(TIMESHEET_3, timesheet);
     }
 
     @Test
     public void created_timesheet_has_expected_values_when_retrieved() {
         Timesheet timesheet = dao.createTimesheet(TIMESHEET_3);
+        assertEquals(6, timesheet.getTimesheetId());
+        assertEquals(TIMESHEET_3.getEmployeeId(), timesheet.getEmployeeId());
+        assertEquals(TIMESHEET_3.getProjectId(), timesheet.getProjectId());
+        assertEquals(TIMESHEET_3.getDateWorked(), timesheet.getDateWorked());
+        assertEquals(TIMESHEET_3.getHoursWorked(), timesheet.getHoursWorked(), 0.001);
+        assertEquals(TIMESHEET_3.isBillable(), timesheet.isBillable());
         assertEquals(TIMESHEET_3.getDescription(), timesheet.getDescription());
     }
 
     @Test
     public void updated_timesheet_has_expected_values_when_retrieved() {
+        assertEquals("Timesheet 4", dao.getTimesheet(4).getDescription());
         TIMESHEET_4.setDescription("Hi, my name is Brandon");
         dao.updateTimesheet(TIMESHEET_4);
         assertEquals("Hi, my name is Brandon", dao.getTimesheet(4).getDescription());
@@ -73,13 +97,15 @@ public class JdbcTimesheetDaoTests extends BaseDaoTests {
 
     @Test
     public void deleted_timesheet_cant_be_retrieved() {
+        assertTimesheetsMatch(TIMESHEET_1, dao.getTimesheet(1));
         dao.deleteTimesheet(1);
         assertEquals(null, dao.getTimesheet(1));
     }
 
     @Test
     public void getBillableHours_returns_correct_total() {
-        assertEquals(.25, dao.getBillableHours(2, 1));
+        assertEquals(.25, dao.getBillableHours(2, 1), 0.001);
+        assertEquals(0, dao.getBillableHours(2, 2), 0.001);
     }
 
     private void assertTimesheetsMatch(Timesheet expected, Timesheet actual) {
